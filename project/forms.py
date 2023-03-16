8b2dea071e247e117594bd655b268a7bce9566bf
Cao Tien Hai <47495928+caotienhai@users.noreply.github.com>
2023-03-16 08:46:41 +0700

    datetimePicker

--- project/forms.py
+++ project/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from crispy_forms.helper import FormHelper
+from bootstrap_datepicker_plus.widgets import DatePickerInput
 from django.forms import inlineformset_factory
 from .models import Project, Task
 
@@ -7,16 +8,15 @@ class TaskRegistrationForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('assign','task_name','task_target','dead_line','task_update','complete_per','status','due')
- 
+        widgets = {
+            "dead_line": DatePickerInput(),}
+        
 class ProjectRegistrationForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = '__all__'
-    def __init__(self, *args, **kwargs):
-            # Add a FormHelper
-        self.helper = FormHelper()
-
-        super(ProjectRegistrationForm, self).__init__(*args, **kwargs)
+        widgets = {
+            "dead_line": DatePickerInput(),}
 
 TaskFormSet = inlineformset_factory(
     Project, Task, form=TaskRegistrationForm, extra=1, can_delete=True, can_delete_extra=True
