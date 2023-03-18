e61f7a4be41e3b8c0d51a008a47e1502c4a6bd6d
Cao Tien Hai <47495928+caotienhai@users.noreply.github.com>
2023-03-18 12:50:54 +0700

    Update context_processors.py

--- team/context_processors.py
+++ team/context_processors.py
@@ -2,7 +2,7 @@ from .models import Team
 
 def team(request):
     if request.user.is_authenticated:
-        team = Team.objects.filter(members__in=request.user)[0]
+        team = Team.objects.filter(members__id=request.user.id)[0]
     else:
         team = None
     return {'team':team}
