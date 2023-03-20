9a3fe63722954714506c149622bd9edea017e8cc
Cao Tien Hai <47495928+caotienhai@users.noreply.github.com>
2023-03-20 14:28:31 +0700

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,7 +8,7 @@ from project.models import Project
 
 @login_required
 def dashboard(request):
-    team = Team.objects.filter(created_by=request.user)[0]
+    team = Team.objects.filter(members__id=request.user.id)[0]
     leads = Lead.objects.filter(team=team, converted_to_client = False).order_by('-created_at')[0:5]
     clients = Client.objects.filter(team=team).order_by('-created_at')[0:5]
     orders = Order.objects.filter(pic=request.user).order_by('-order_date')[0:5].select_related('client')
