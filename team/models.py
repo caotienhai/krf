11349c9246b129827000ab91e570d1b3421aaf85
Cao Tien Hai <47495928+caotienhai@users.noreply.github.com>
2023-03-23 15:58:35 +0700

    cached

--- team/models.py
+++ team/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Team(models.Model):    
     name = models.CharField(max_length=100)
-    members = models.ManyToManyField(User,related_name='teams', null=True, blank=True)
+    members = models.ManyToManyField(User,related_name='teams', blank=True)
     created_by = models.ForeignKey(User,related_name='created_teams',on_delete=models.CASCADE,null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
     
