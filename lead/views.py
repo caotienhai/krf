626707909ce8f0f29a11dd510a37ad22b490f861
Cao Tien Hai <47495928+caotienhai@users.noreply.github.com>
2023-03-28 16:29:51 +0700

    cd

--- lead/views.py
+++ lead/views.py
@@ -248,6 +248,35 @@ class ContactList(LoginRequiredMixin,FilterView):
         else:
             return contacts.filter(assign_to=self.request.user).order_by('lead')
 
+class ContactDetailView(LoginRequiredMixin,DetailView): 
+    model = Contact
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Contact Detail'
+        return context
+        
+    def get_queryset(self):
+        queryset = super(ContactDetailView, self).get_queryset()
+        return queryset.filter(pk=self.kwargs.get('pk'))
+
+class ContactUpdateView(LoginRequiredMixin,UpdateView):
+    model = Contact    
+    fields = ['first_name','last_name','lead',
+                  'birth_date','married','family','phone_number','email','religion',
+                  'disc','gains','pains','stakeholders',
+                  'assign_to','team']
+    success_url = reverse_lazy('leads:contact')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Update Contact'
+        return context
+    
+    def get_queryset(self):
+        queryset = super(ContactUpdateView, self).get_queryset()
+        return queryset.filter(pk=self.kwargs.get('pk'))
+    
 @login_required
 def delete_contact(request, pk):    
     contact = Contact.objects.get(id=pk)
